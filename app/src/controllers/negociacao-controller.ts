d2f26d1b1de32822874d1258e0860133c377430a
Hallison <hallison.o.b@gmail.com>
2023-06-15 18:23:22 -0300

    feat: decorators

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -1,24 +1,31 @@
-import { DiasDaSemana } from "../enums/dias-da-semana.js";
+import { inspect } from "../decorators/inspect.js";
 import { Negociacao } from "../models/negociacao.js";
+import { domInjector } from "../decorators/dom-injector.js";
 import { Negociacoes } from "../models/negociacoes.js";
-import { MensagemView } from "../views/mensagem.view.js";
-import { NegociacoesView } from "../views/negociacoes.view.js";
+import { MensagemView } from "../views/mensagem-view.js";
+import { DiasDaSemana } from "../enums/dias-da-semana.js";
+import { NegociacoesView } from "../views/negociacoes-view.js";
+import { logarTempoDeExecucao } from "../decorators/logar-tempo-execucao.js";
 
 export class NegociacaoController {
+  @domInjector("#data")
   private inputData: HTMLInputElement;
+
+  @domInjector("#quantidade")
   private inputQuantidade: HTMLInputElement;
+
+  @domInjector("#valor")
   private inputValor: HTMLInputElement;
+
   private negociacoes = new Negociacoes();
-  private negociacoesView = new NegociacoesView("#negociacoesView", true);
-  private mensagemView = new MensagemView("#mensagemView", false);
+  private negociacoesView = new NegociacoesView("#negociacoesView");
+  private mensagemView = new MensagemView("#mensagemView");
 
   constructor() {
-    this.inputData = document.querySelector("#data");
-    this.inputQuantidade = document.querySelector("#quantidade");
-    this.inputValor = document.querySelector("#valor");
     this.negociacoesView.update(this.negociacoes);
   }
-
+  @inspect
+  @logarTempoDeExecucao()
   public adiciona(): void {
     const negociacao = Negociacao.criaDe(
       this.inputData.value,
@@ -26,12 +33,21 @@ export class NegociacaoController {
       this.inputValor.value
     );
 
-    if (!this.diaUtil(negociacao.data)) {
+    if (!this.ehDiaUtil(negociacao.data)) {
       this.mensagemView.update("Apenas negociações em dias úteis são aceitas");
+      return;
     }
+
     this.negociacoes.adiciona(negociacao);
     this.limparFormulario();
     this.atualizaView();
+  }
+
+  private ehDiaUtil(data: Date) {
+    return (
+      data.getDay() > DiasDaSemana.DOMINGO &&
+      data.getDay() < DiasDaSemana.SABADO
+    );
   }
 
   private limparFormulario(): void {
@@ -44,12 +60,5 @@ export class NegociacaoController {
   private atualizaView(): void {
     this.negociacoesView.update(this.negociacoes);
     this.mensagemView.update("Negociação adicionada com sucesso");
-  }
-
-  private diaUtil(data: Date): boolean {
-    return (
-      data.getDate() > DiasDaSemana.DOMINGO &&
-      data.getDate() < DiasDaSemana.SABADO
-    );
   }
 }
--- app/src/views/negociacoes-view.ts
+++ app/src/views/negociacoes-view.ts
@@ -1,7 +1,9 @@
+import { escapar } from "../decorators/escape.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { View } from "./view.js";
 
 export class NegociacoesView extends View<Negociacoes> {
+  @escapar
   protected template(model: Negociacoes): string {
     return `
         <table class="table table-hover table-bordered">
@@ -18,16 +20,21 @@ export class NegociacoesView extends View<Negociacoes> {
                   .map((negociacao) => {
                     return `
                         <tr>
-                            <td>${this.formatar(negociacao.data)}</td>
-                            <td>${negociacao.quantidade}</td>
-                            <td>${negociacao.valor}</td>
+                            <td>${this.formatar(negociacao.data)}
+                            </td>
+                            <td>
+                                ${negociacao.quantidade}
+                            </td>
+                            <td>
+                                ${negociacao.valor}
+                            </td>
                         </tr>
                     `;
                   })
                   .join("")}
             </tbody>
         </table>
-    `;
+        `;
   }
 
   private formatar(data: Date): string {
